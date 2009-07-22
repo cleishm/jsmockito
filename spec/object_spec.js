a1b88b54afaa6f804bff8be5e186188b9c57d690
Chris Leishman <chris@leishman.org>
2009-07-23 01:31:11 +0530

    Added stubbing of object methods

--- spec/object_spec.js
+++ spec/object_spec.js
@@ -1,9 +1,14 @@
 // vi:ts=2 sw=2 expandtab
 Screw.Unit(function() {
   describe('JsMockito object mocking', function() {
-    var MyObject = function() {
-      this.greeting = function() { return "hello" };
-    };
+    var MyObject;
+
+    before(function() {
+      MyObject = function() {
+        this.greeting = function() { return "hello" };
+        this.farewell = function() { return "goodbye" };
+      };
+    });
 
     describe("when mock object created", function() {
       var mockObj;
@@ -25,7 +30,7 @@ Screw.Unit(function() {
       });
 
       it("should return undefined", function() {
-        assertThat(result, equalTo(undefined));
+        assertThat(result, sameAs(undefined));
       });
 
       it("should verify method was invoked", function() {
@@ -66,13 +71,60 @@ Screw.Unit(function() {
       });
     });
 
+    describe("when mock method invocked with multiple arguments", function() {
+      var mockObj;
+      before(function() {
+        mockObj = mock(MyObject);
+        mockObj.farewell('hunter', 'thompson', 67);
+      });
+
+      it("should verify the method was invoked", function() {
+        verify(mockObj).farewell();
+      });
+
+      it("should verify the method was invoked with some arguments", function() {
+        verify(mockObj).farewell('hunter', 'thompson');
+      });
+
+      it("should verify the method was invoked with all arguments", function() {
+        verify(mockObj).farewell('hunter', 'thompson', 67);
+      });
+
+      it("should verify the method was invoked using matchers", function() {
+        verify(mockObj).farewell('hunter', 'thompson', lessThan(100));
+      });
+
+      it("should not verify the method was invoked if looking for additional arguments", function() {
+        var exception;
+        try { 
+          verify(mockObj).farewell('hunter', 'thompson', 67, 'batcountry');
+        } catch (err) {
+          exception = err;
+        }
+        assertThat(exception, not(nil()), "Exception not raised");
+        assertThat(exception, equalTo(
+          "Wanted but not invoked: obj.farewell(<equal to \"hunter\">, <equal to \"thompson\">, <equal to 67>, <equal to \"batcountry\">)"));
+      });
+
+      it("should not verify the method was invoked if different arguments", function() {
+        var exception;
+        try { 
+          verify(mockObj).farewell('hunter', 'thompson', 68);
+        } catch (err) {
+          exception = err;
+        }
+        assertThat(exception, not(nil()), "Exception not raised");
+        assertThat(exception, equalTo(
+          "Wanted but not invoked: obj.farewell(<equal to \"hunter\">, <equal to \"thompson\">, <equal to 68>)"));
+      });
+    });
+
     describe("when mock method invocked with different scope", function() {
       var mockObj;
       var testScope = {};
-      var result;
       before(function() {
         mockObj = mock(MyObject);
-        result = mockObj.greeting.call(testScope);
+        mockObj.greeting.call(testScope);
       });
 
       it("should not verify that the method was invoked without explicit scope", function() {
@@ -89,6 +141,113 @@ Screw.Unit(function() {
 
       it("should verify method was invoked with explicit scope", function() {
         verify(mockObj).greeting.apply(testScope, []);
+      });
+    });
+
+    describe("when stubbing methods", function() {
+      var mockObj;
+      before(function() {
+        mockObj = mock(MyObject);
+      });
+
+      var stubScope;
+      var stubArguments;
+      function stubFunction() {
+        stubScope = this;
+        stubArguments = arguments;
+        return 'stub result';
+      }
+
+      after(function() {
+        stubScope = undefined;
+        stubArguments = undefined;
+      });
+
+      describe("when method is stubbed with no arguments", function() {
+        describe("when no clause applied", function() {
+          before(function() {
+            when(mockObj).greeting();
+          });
+
+          it("should return undefined", function() {
+            assertThat(mockObj.greeting(), sameAs(undefined));
+          });
+        });
+
+        describe("when using 'then' and a function stub", function() {
+          before(function() {
+            when(mockObj).greeting().then(stubFunction);
+          });
+
+          it("should return result of stub function", function() {
+            assertThat(mockObj.greeting(), equalTo('stub result'));
+          });
+
+          it("should invoke stub function when called", function() {
+            mockObj.greeting();
+            assertThat(stubArguments, not(nil()));
+          });
+
+          it("should invoke stub function with the mock as scope by default", function() {
+            mockObj.greeting();
+            assertThat(stubScope, sameAs(mockObj), "Scope was not the same");
+          });
+
+          it("should invoke stub function with the same arguments", function() {
+            mockObj.greeting('hello', undefined, 5);
+            assertThat(stubArguments, equalTo(['hello', undefined, 5]));
+          });
+
+          it("should invoke stub function when invoked via call with object as scope", function() {
+            mockObj.greeting.call(mockObj);
+            assertThat(stubScope, sameAs(mockObj), "Scope was not the same");
+          });
+
+          it("should invoke stub function when invoked via apply with object as scope", function() {
+            mockObj.greeting.apply(mockObj, ['hello', 6]);
+            assertThat(stubScope, sameAs(mockObj), "Scope was not the same");
+            assertThat(stubArguments, equalTo(['hello', 6]));
+          });
+
+          it("should not invoke stub function when invoked via call with different scope", function() {
+            assertThat(mockObj.greeting.call({}), sameAs(undefined));
+            assertThat(stubScope, sameAs(undefined));
+          });
+        });
+      });
+
+      describe("when method is stubbed with multiple arguments", function() {
+        before(function() {
+          when(mockObj).farewell('foo', lessThan(10), anything()).then(stubFunction);
+        });
+
+        it("should return result of stub function", function() {
+          assertThat(mockObj.farewell('foo', 9, {}), equalTo('stub result'));
+        });
+
+        it("should invoke stub even if additional arguments are present", function() {
+          assertThat(mockObj.farewell.apply(mockObj, ['foo', 9, {}, 'something else']), equalTo('stub result'));
+        });
+
+        it("should return undefined if insufficent arguments compared to stub", function() {
+          assertThat(mockObj.farewell('foo', 9), sameAs(undefined));
+        });
+
+        it("should return undefined if arguments do not match", function() {
+          assertThat(mockObj.farewell('foo', 11, 'bar'), sameAs(undefined));
+        });
+      });
+    
+      describe("when stubbing a method with explit scope matcher and 'then' clause", function() {
+        before(function() {
+          when(mockObj).greeting.call(anything()).then(stubFunction);
+        });
+
+        it("should invoke stub function with the same explicit scope", function() {
+          var scope = {};
+          mockObj.greeting.call(scope, 1, 'foo');
+          assertThat(stubScope, sameAs(scope), "Scope was not the same");
+        });
       });
     });
   });
--- src/object.js
+++ src/object.js
@@ -3,7 +3,27 @@
 /**
  * Create a mockable and stubbable objects.
  *
+ * A mock is created with the constructor for an object as an argument.  Once
+ * created, the mock object will have all the same methods as the source object
+ * which, when invoked, will return undefined by default.
+ *
+ * Stub declarations may then be made for these methods to have them return
+ * useful values or perform actions when invoked.
+ *
  * <pre>
+ * MyObject = function() {
+ *   this.add = function(a, b) { return a + b }
+ * };
+ *
+ * var mockObj = JsMockito.mock(MyObject);
+ * mockFunc(5, 4); // result is undefined
+ *
+ * JsMockito.when(mockFunc).add(1, 2).thenReturn(6);
+ * mockFunc(1, 2); // result is 6
+ *
+ * JsMockito.verify(mockFunc)(1, greaterThan(2)); // ok
+ * JsMockito.verify(mockFunc)(1, equalTo(2)); // ok
+ * JsMockito.verify(mockFunc)(1, 4); // will throw an exception
  * </pre>
  *
  * @return {function} an mock object
@@ -14,12 +34,14 @@ JsMockito.mock = function(Obj) {
   MockObject.prototype.constructor = MockObject;
 
   var mockObject = new MockObject();
+  mockObject._jsMockitoStubBuilder = {};
   mockObject._jsMockitoVerifier = {};
 
   for (var name in mockObject) (function(name) {
     if (name == 'constructor')
       return;
     mockObject[name] = JsMockito.mockFunction('obj.' + name, mockObject);
+    mockObject._jsMockitoStubBuilder[name] = mockObject[name]._jsMockitoStubBuilder;
     mockObject._jsMockitoVerifier[name] = mockObject[name]._jsMockitoVerifier;
   })(name);
 
